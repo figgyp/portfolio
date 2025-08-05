f4707747d4e560165a1a9f52176d2fcacdd2baab
James FP <174389827+figgyp@users.noreply.github.com>
2025-08-05 19:49:03 -0400

    hack together client router

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,24 @@
+import { Link, Route, Router, Switch, useLocation } from "wouter";
+import { TestimonialCard } from "@/testimonial-card/testimonial-card.tsx";
+import { usePathname } from "wouter/use-browser-location";
+
 function App() {
+  const [path] = usePathname();
+  const [location] = useLocation();
+  console.log(path);
+  console.log(location);
+
   return (
-    <main className="m-10">
-      <h1 className="text-3xl text-green-500 font-bold underline">
-        Hello, world!
-      </h1>
-    </main>
+    <Router base="/portfolio">
+      <main className="m-10">
+        {location === "/portfolio/" && (
+          <Link href="/testimonial-card">Testimonial Card</Link>
+        )}
+        <Switch>
+          <Route path="/testimonial-card" component={TestimonialCard} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
---
+++ src/testimonial-card/testimonial-card.tsx
@@ -0,0 +1,9 @@
+function TestimonialCard() {
+  return (
+    <h1 className="text-3xl text-green-500 font-bold underline">
+      Hello, world!
+    </h1>
+  );
+}
+
+export { TestimonialCard };
