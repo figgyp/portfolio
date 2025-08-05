b6dc859237865bdc965052bc8641c7f7728bab46
James FP <174389827+figgyp@users.noreply.github.com>
2025-08-05 19:52:29 -0400

    fix image in build;

--- src/testimonial-card/testimonial-card-demo.tsx
+++ src/testimonial-card/testimonial-card-demo.tsx
@@ -1,10 +1,11 @@
 import { TestimonialCard } from "@/testimonial-card/testimonial-card";
+import profileThumbnail from "./assets/profile-thumbnail.png";
 
 const testimonial = {
   name: "Sarah Dole",
   login: "@sarahdole",
   copy: "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
-  avatarUrl: "./assets/profile-thumbnail.png",
+  avatarUrl: profileThumbnail,
 };
 
 function TestimonialCardDemo() {
